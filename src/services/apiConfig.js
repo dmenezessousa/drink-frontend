499b82ae3f2bbe58c5f3f969daf61d93a11e67ee
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-10 14:54:05 -0400

    replaces leftover product with drink

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -10,7 +10,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://sei-products-api.herokuapp.com/api"
-      : "http://localhost:3000/api",
+      : "http://localhost:3000/api/",
 });
 
 api.interceptors.request.use(
