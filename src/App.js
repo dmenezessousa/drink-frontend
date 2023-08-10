76c256bcf8ce29da10bcfa909aace204bb19161d
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-10 13:49:48 -0400

    restructures general file structure with auth example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import Nav from './components/Nav.jsx';
-import Home from './screens/Home.jsx';
-import Drinks from './screens/Drinks.jsx';
-import DrinkDetail from './screens/DrinkDetail.jsx';
-import DrinkCreate from './screens/DrinkCreate.jsx';
-import DrinkEdit from './screens/DrinkEdit.jsx';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./screens/Home/Home.jsx";
+import Products from "./screens/Products/Products.jsx";
+import ProductCreate from "./screens/ProductCreate/ProductCreate.jsx";
+import ProductEdit from "./screens/ProductEdit/ProductEdit.jsx";
+import ProductDetail from "./screens/ProductDetail/ProductDetail.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { verifyUser } from "./services/users.js";
+import SignUp from "./screens/SignUp/SignUp.jsx";
+import SignIn from "./screens/SignIn/SignIn.jsx";
+import SignOut from "./screens/SignOut/SignOut.jsx";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
   return (
-    <div className="App">
-      <Nav />
+    <div className="app">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/drinks" element={<Drinks />} />
-        <Route path="/drinks/:id" element={<DrinkDetail />} />
-        <Route path="/add-drink" element={<DrinkCreate />} />
-        <Route path="/drinks/:id/edit" element={<DrinkEdit />} />
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/sign-up" element={<SignUp setUser={setUser} />} />
+        <Route path="/sign-in" element={<SignIn setUser={setUser} />} />
+        <Route path="/sign-out" element={<SignOut setUser={setUser} />} />
+        <Route path="/products" element={<Products user={user} />} />
+        <Route
+          path="/add-product"
+          element={
+            user ? <ProductCreate user={user} /> : <Navigate to="/sign-up" />
+          }
+        />
+        <Route
+          path="/products/:id/edit"
+          element={user ? <ProductEdit user={user} /> : <Navigate to="/" />}
+        />
+        <Route path="/products/:id" element={<ProductDetail user={user} />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,20 +1,27 @@
 import axios from "axios";
 
-let apiUrl
-
-const apiUrls = {
-  production: "www.cat-crud-api.heroku.com/api",
-  development: "http://localhost:3000/api"
-}
-
-if (window.location.hostname === "localhost") {
-  apiUrl = apiUrls.development
-} else {
-  apiUrl = apiUrls.production
-}
+const getToken = () => {
+  return new Promise((resolve) => {
+    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+  });
+};
 
 const api = axios.create({
-  baseURL: apiUrl
-})
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://sei-products-api.herokuapp.com/api"
+      : "http://localhost:3000/api",
+});
 
-export default api
+api.interceptors.request.use(
+  async function (config) {
+    config.headers["Authorization"] = await getToken();
+    return config;
+  },
+  function (error) {
+    console.log("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
